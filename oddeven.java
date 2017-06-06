90844b5a08efd45159d516a2bcb2c2a0ac6862bf
sridharanv09 <sridharsri21041997@gmail.com>
2017-06-06 15:07:25 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+import java util.scanner;
+public class oddeven{
+public static void main(String []args){
+int n;
+System.out.println("enter the number:");
+Scanner in=new Scanner(System.in);
+Scanner n=new Scanner(System.in);
+if(n%2==0){
+System.out.println("even number:");
+else
+{System.out.println("odd number:");
+}
+}
