eea14bdcc31f0a9d704316441b893a39bd02d03a
sridharanv09 <sridharsri21041997@gmail.com>
2017-06-06 14:58:10 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,18 @@
+
+import java util.Scanner;
+public class 
+{
+public static void main(String []args)
+{
+int a;
+System.out.println("enter the number:");
+Scanner in=new Scanner(System.in);
+Scanner a=new Scanner(System.in);
+if(a>0){
+System.out.println("the number is positive:");}
+if(a<0){
+System.out.println("the number is negative:");}
+if(a==0){
+System.out.println("the number is zero:");}
+}
+}
