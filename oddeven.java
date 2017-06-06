a0ef128e3ff42055e2c7ac20bf49bf701d79665b
sridharanv09 <sridharsri21041997@gmail.com>
2017-06-06 15:10:15 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -6,7 +6,7 @@ System.out.println("enter the number:");
 Scanner in=new Scanner(System.in);
 Scanner n=new Scanner(System.in);
 if(n%2==0){
-System.out.println("even number:");
+System.out.println("even number:");}
 else
 {System.out.println("odd number:");
 }
