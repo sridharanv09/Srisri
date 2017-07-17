575fdb182a3c2b3235299d82b101e3c7d47a0aaa
sridharanv09 <sridharsri21041997@gmail.com>
2017-07-17 09:28:26 +0530

    Add files via upload

--- 22.java
+++ 22.java
@@ -1,38 +1,25 @@
- import java.io.*;
-class LcmMethod_2
-{
-public static void main(String args[])throws IOException
-{
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-int a,b,max,min,x,lcm=1;
-System.out.print("Enter the 1st number : ");
-a=Integer.parseInt(br.readLine());
-System.out.print("Enter the 2nd number : ");
-b=Integer.parseInt(br.readLine());
- 
- 
-if(a>b)
-{
-max=a;
-min=b;
-}
-else
-{
-max=b;
-min=a;
-}
- 
- 
- 
-for(int i=1;i<=min;i++)
-   {
-    x=max*i;  
-    if(x%min==0)  
-     {
-      lcm=x;  
-      break; 
-     }
+
+import java.io.*;
+class Hcf
+    {
+        public static void main(String args[])throws IOException
+        {
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter the First no : ");
+            int n1=Integer.parseInt(br.readLine());
+            System.out.print("Enter the Second no : ");
+            int n2=Integer.parseInt(br.readLine());
+            int hcf=0;
+            int min = Math.min(n1,n2);
+             
+            for(int i=min; i >= 1; i--)
+            {
+                if(n1%i == 0 && n2%i == 0)
+                {
+                    hcf = i;
+                    break;
+                }
+            }        
+            System.out.print("\nThe hcf of "+n1+" and "+n2+" = "+hcf);
+        }
     }
-System.out.println("L.C.M. = "+lcm);
-}
-}
