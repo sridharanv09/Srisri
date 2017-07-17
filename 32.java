e22abba7998e3638acb8409ea7d3798e4aa80b83
sridharanv09 <sridharsri21041997@gmail.com>
2017-07-17 10:41:38 +0530

    Add files via upload

---
+++ 32.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class cat1
+{
+  public static void main(String args[])
+  {
+    String st;
+    Scanner sc=new Scanner(System.in);
+    st=nextLine();
+    String str1[]=st.split(" ");
+    System.out.println(str1.length+"is word");
+  }
+}
